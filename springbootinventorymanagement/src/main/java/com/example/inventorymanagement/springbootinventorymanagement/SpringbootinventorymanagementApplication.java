cce5e7234a4812fd2e0620905420c394a99cc86d
Latha <lattanaidu@gmail.com>
2021-07-29 19:25:21 +0530

    first commit

---
+++ springbootinventorymanagement/src/main/java/com/example/inventorymanagement/springbootinventorymanagement/SpringbootinventorymanagementApplication.java
@@ -0,0 +1,13 @@
+package com.example.inventorymanagement.springbootinventorymanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootinventorymanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootinventorymanagementApplication.class, args);
+	}
+
+}
---
+++ springbootinventorymanagement/src/test/java/com/example/inventorymanagement/springbootinventorymanagement/SpringbootinventorymanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.inventorymanagement.springbootinventorymanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootinventorymanagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
